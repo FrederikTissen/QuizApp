46cc3e06c5447405d9a7e1be6ee1725deda97ab0
FrederikTissen <frederiktissen@web.de>
2022-11-14 12:53:14 +0100

    data update

---
+++ QuizApp/data.js
@@ -0,0 +1,141 @@
+let categories = [
+    'Computer', 'Internet', 'Handy & Co.', 'Software'
+]
+
+let questions = [
+    {
+        "category": 0,
+        "question": "Wer hat den Computer erfunden?",
+        "answer_1": "Michael Ballack",
+        "answer_2": "Das Sams",
+        "answer_3": "Konrad Zuse",
+        "answer_4": "König Ludwig XIV",
+        "correctAnswer": 2
+    },
+    {
+        "category": 0,
+        "question": "Wieviel Bit stecken in einem Kilobyte?",
+        "answer_1": "1000",
+        "answer_2": "8192",
+        "answer_3": "80000",
+        "answer_4": "13",
+        "correctAnswer": 1
+    },
+    {
+        "category": 0,
+        "question": "Wie schnell war der erste Computer?",
+        "answer_1": "1300 Hz",
+        "answer_2": "1 Hz",
+        "answer_3": "16 kHz",
+        "answer_4": "5,3 Hz",
+        "correctAnswer": 3
+    },
+    {
+        "category": 0,
+        "question": "Wann gab es den ersten PC?",
+        "answer_1":    "1938 (Zuse Z3)",
+        "answer_2":    "1976 (Apple I)",
+        "answer_3":    "1966 (PC-4712)",
+        "answer_4":    "1991 (BlueScreen)",
+        "correctAnswer": 1
+    },
+    {
+        "category": 0,
+        "question": "Welche Dezimalzahl entspricht dem BinÃ¤rcode 101010?",
+        "answer_1": "1111",
+        "answer_2": "13",
+        "answer_3": "42",
+        "answer_4": "499",
+        "correctAnswer": 2
+    },
+    {
+        "category": 1,
+        "question": "Wer ist der Erfinder der 'Internetsprache' HTML?",
+        "answer_1": "Tim Berners-Lee",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Didi Hallervorden",
+        "answer_4": "Stephen Hawking",
+        "correctAnswer": 0
+    },
+    {
+        "category": 1,
+        "question": "Was bedeutet HTTPS?",
+        "answer_1": "Hypertext Transfer Protocol",
+        "answer_2": "High Tension Titan-Poly Socket",
+        "answer_3": "Hypertext Transport Protocol Safety",
+        "answer_4": "Hypertext Transfer Protocol Secure",
+        "correctAnswer": 3
+    },
+    {
+        "category": 2,
+        "question": "Was ist 5G?",
+        "answer_1": "Ein Elektroauto von Tesla",
+        "answer_2": "Ein Mobilfunkstandard",
+        "answer_3": "Ein Handymodell von Nokia",
+        "answer_4": "Eine hochfeste Stahlsorte",
+        "correctAnswer": 1
+    },
+    {
+        "category": 2,
+        "question": "Welche Firma hat das erste Handy erfunden?",
+        "answer_1": "Siemens",
+        "answer_2": "Nokia",
+        "answer_3": "Motorola",
+        "answer_4": "Privileg",
+        "correctAnswer": 2
+    },
+    {
+        "category": 2,
+        "question": "Wieviel wog das erste Handy?",
+        "answer_1": "3,6 kg",
+        "answer_2": "800 g",
+        "answer_3": "10 kg",
+        "answer_4": "1 kg",
+        "correctAnswer": 3
+    },
+    {
+        "category": 2,
+        "question": "Wie lange konnte man mit dem ersten Handy telefonieren?",
+        "answer_1": "6 Minuten 30 Sekunden",
+        "answer_2": "20 Minuten",
+        "answer_3": "3 Stunden",
+        "answer_4": "1,5 Stunden",
+        "correctAnswer": 1
+    },
+    {
+        "category": 3,
+        "question": "Wer entwickelte das Betriebssystem Windows?",
+        "answer_1": "Tim Berners-Lee",
+        "answer_2": "Alfred Biolek",
+        "answer_3": "Microsoft",
+        "answer_4": "Captain Kirk",
+        "correctAnswer": 2
+    },
+    {
+        "category": 3,
+        "question": "Was ist Java?",
+        "answer_1": "Eine Insel im Pazifik",
+        "answer_2": "Eine Programmiersprache",
+        "answer_3": "Eine Teesorte",
+        "answer_4": "Eine Modemarke",
+        "correctAnswer": 1
+    },
+    {
+        "category": 3,
+        "question": "Was ist ein Bug?",
+        "answer_1": "Eine spezielle Programmfunktion",
+        "answer_2": "Ein Tastaturkürzel",
+        "answer_3": "Ein Programmierfehler",
+        "answer_4": "Eine andere Bezeichnung für 'App'",
+        "correctAnswer": 2
+    },
+    {
+        "category": 3,
+        "question": "Welche Programmiersprache verwendet nur die Zeichen +-&lt;&gt;[],.?",
+        "answer_1": "C++",
+        "answer_2": "MS-DOS",
+        "answer_3": "Brainf*ck",
+        "answer_4": "ASCII",
+        "correctAnswer": 2
+    },
+]
